85d9af037418aa6a050b12697546c4660fa15c7c
Harish D <harishd5499@gmail.com>
2026-07-09 09:12:57 +0530

    Create 67-AddBinary

---
+++ 67-AddBinary/67-AddBinary.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:12:54 AM
+import java.math.BigInteger;
+class Solution {
+    public String addBinary(String a, String b) {
+        BigInteger num1 = new BigInteger(a, 2);
+        BigInteger num2 = new BigInteger(b, 2);
+        BigInteger sum = num1.add(num2);
+
+        return sum.toString(2);
+    }
+}
