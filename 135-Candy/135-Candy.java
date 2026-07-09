7bf14666c1a898a7b940ea0d194740393db95ca6
Harish D <harishd5499@gmail.com>
2026-07-09 09:12:41 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.java
@@ -0,0 +1,22 @@
+// Last updated: 7/9/2026, 9:12:38 AM
+class Solution {
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] candies = new int[n];
+        for (int i=1; i<n; i++) {
+            if (ratings[i]>ratings[i-1]) {
+                candies[i]=candies[i-1]+1;
+            }
+        }
+        for (int i=n-2; i>=0; i--) {
+            if (ratings[i]>ratings[i+1]) {
+                candies[i]= Math.max(candies[i], candies[i+1]+1);
+            }
+        }
+        int ans=0;
+        for (int i: candies) {
+            ans+=i+1;
+        }
+        return ans;
+    }
+}
